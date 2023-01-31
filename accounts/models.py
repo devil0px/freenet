d072dda05a6039e5845f1416254ffe21517b2363
devil0px <devil0px@gmail.com>
2023-01-31 10:06:21 +0700

    add_fix_accounts

--- accounts/models.py
+++ accounts/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils.translation import gettext as _
-
+from utils.generate_code import generaste_code
 
 
 class Profile(models.Model):

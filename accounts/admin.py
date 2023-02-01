e7a772901287ca7f0956f2136dd205525d008fef
devil0px <devil0px@gmail.com>
2023-02-01 09:54:12 +0700

    add_admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,12 @@
+from atexit import register
 from django.contrib import admin
 
 # Register your models here.
+from .models import Profile , UserPhoneNumber
+
+
+
+
+admin.site.register(Profile)
+#admin.site.register(UserAddress)
+admin.site.register(UserPhoneNumber)

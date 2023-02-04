d0a160d3611ca8e13276218fe89bf3b954efb133
devil0px <devil0px@gmail.com>
2023-02-04 09:49:15 +0700

    add_urls

---
+++ accounts/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path 
+from .views import signup , user_activate , profile , wishlist
+
+app_name='accounts'
+
+
+
+
+urlpatterns = [
+    #path('test',test_celery),
+    #path('dashboard',dashboard),
+    path('signup/' , signup , name='signup'),
+    path('profile/' , profile , name='profile'),
+    path('profile/wishlist' , wishlist , name='wishlist'),
+    path('<str:username>/activate' , user_activate , name='user_activate'),
+
+]
